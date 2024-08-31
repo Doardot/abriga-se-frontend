2eada7c3d9d7a08d62ec8df040d1165baeeb5e53
Inacio Pimentel <inacio.frick@edu.pucrs.br>
2024-08-31 08:58:25 -0300

    fix: visual problems

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from './App.tsx'
 import './index.css'
-import { ConfigProvider, Modal } from "antd";
+import { ConfigProvider } from "antd";
 import Router from "./router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 
-  <ConfigProvider direction="rtl">
+  <ConfigProvider>
     <Router />
   </ConfigProvider>
 );
