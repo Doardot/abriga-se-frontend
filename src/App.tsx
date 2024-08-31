747d46ab01d371b22633172f68f732c3c936074f
Eduardo Wolf <eduardomartiniwolf@gmail.com>
2024-08-31 09:22:05 -0300

    [feat]: register screen done

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Header from './components/header/Header'
 import LoginPage from './components/header/loginPage/index.tsx'
 import { LandingPage } from './api/landing-page/pages/LandingPage'
 import ModalPessoas from './components/ModalPessoas'
+import RegisterShelterPage from  './components/ShelterRegistrationPage/index.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,8 +14,7 @@ function App() {
   return (
 
     <div style={{ width: '100%' }}>
-      <LandingPage />
-      <ModalPessoas />
+      <RegisterShelterPage/>
     </div>
   )
 }
