421924875739ebc37df8293515711496172485f1
Eduardo Wolf <eduardomartiniwolf@gmail.com>
2024-08-31 07:16:12 -0300

    feat/Login-page

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/Header'
+import LoginPage from './components/header/loginPage/index.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
-  <div style={{width: '100%'}}>
-    <Header></Header>
 
-  </div>
-    
-    
+    <div style={{ width: '100%' }}>
+      <LoginPage></LoginPage>
+
+    </div>
+
+
   )
 }
 
